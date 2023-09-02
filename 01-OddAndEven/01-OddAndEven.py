3d77e96698a71e036f7ca4da88523b13dfb6f973
Payamkhoshhal <payam.khooshhall@gmail.com>
2023-09-02 14:09:51 +0200

    01-OddAndEven problem'

---
+++ 01-OddAndEven/01-OddAndEven.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int (input().strip()) #strip removes the spaces from the start and end
+    if n % 2 != 0:
+        print('Weird')
+    else:
+        if n in range(2, 6) or n > 20:
+            print('Not Weird')
+        elif n in range(6, 21):
+            print('Wierd')
