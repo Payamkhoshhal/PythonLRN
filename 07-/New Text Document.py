fa59e86c0c25142cc6fa62477090e034bf25210f
Payamkhoshhal <payam.khooshhall@gmail.com>
2023-09-06 17:08:59 +0200

    delete 07

--- 07-/New Text Document.py
+++
@@ -1,9 +0,0 @@
-regex_integer_in_range = r"\b\d{6}\b"	# Do not delete 'r'.
-regex_alternating_repetitive_digit_pair = r"(\d)(.)\1"	# Do not delete 'r'.
-
-
-import re
-P = input()
-print('++++++++++')
-print(re.findall(regex_integer_in_range, P))
-
